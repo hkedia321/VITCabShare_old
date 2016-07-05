5fbfcf096f2e19d409fdb850659355c7147b2554
Harshit Kedia <harshit.kedia2015@vit.ac.in>
2016-07-06 00:08:40 +0530

    4th commit-mail trying

---
+++ contactus.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once "php/PHPMailerAutoload.php";
+
+$mail = new PHPMailer;
+
+//Enable SMTP debugging. 
+$mail->SMTPDebug = 3;                               
+//Set PHPMailer to use SMTP.
+$mail->isSMTP();            
+//Set SMTP host name                          
+$mail->Host = "smtp.gmail.com";
+//Set this to true if SMTP host requires authentication to send email
+$mail->SMTPAuth = true;                          
+//Provide username and password     
+$mail->Username = "vitcabshare@gmail.com";                 
+$mail->Password = "Aawrun#32";                           
+//If SMTP requires TLS encryption then set it
+$mail->SMTPSecure = "tls";                           
+//Set TCP port to connect to 
+$mail->Port = 587;                                   
+
+$mail->From = "vitcabshare@gmail.com";
+$mail->FromName = "VIT Cab share";
+
+$mail->addAddress("geek.harshitkedia@gmail.com", "Recepient Name");
+
+$mail->isHTML(true);
+
+$mail->Subject = "Subject Text";
+$mail->Body = "<i>Mail body in HTML</i>";
+$mail->AltBody = "This is the plain text version of the email content";
+
+if(!$mail->send()) 
+{
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} 
+else 
+{
+    echo "Message has been sent successfully";
+}
