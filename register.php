d3ae1539e35ceb9d01c003def33e38a935f69c58
Harshit Kedia <harshit.kedia2015@vit.ac.in>
2016-07-07 23:17:43 +0530

    up

--- register.php
+++ register.php
@@ -86,7 +86,7 @@ else {
 
 				<div class="formdiv">
 					<h1 class="text-center">Register</h1>
-					<h4 class="text-center"><span id="badge" class="badge"><span id="badgespan">44</span> registrations!</span></h4>
+					<h4 class="text-center"><span id="badge" class="badge"><span id="badgespan">46</span> registrations!</span></h4>
 					<br>
 					<form action="register.php" method="POST" name="registerform" onsubmit="return validate()">
 						<div class="form-group">
